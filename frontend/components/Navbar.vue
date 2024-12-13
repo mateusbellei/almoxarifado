6e086223adad39ec5b8f55f1d8b330d8807a3d8c
mateusbellei <dev.mbellei@gmail.com>
2024-12-12 22:02:12 -0300

    navbar fixes

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,22 +1,29 @@
 <template>
   <nav
-    class="fixed top-0 left-0 right-0 flex justify-between items-center p-4 bg-transparent transition-all ease-in-out duration-300"
-    :class="{ 'bg-opacity-90': isScrolled }">
-    <NuxtLink to="/" class="flex items-center gap-2 text-2xl font-bold text-blue-500">
+    class="fixed top-0 left-0 right-0 flex justify-between items-center p-4 bg-transparent transition-all ease-in-out duration-300 shadow-lg"
+    :class="{ 'bg-white dark:bg-gray-900': isScrolled }"
+  >
+    <NuxtLink
+      to="/"
+      class="flex items-center gap-2 text-2xl font-bold text-blue-500"
+    >
       <img src="logo.png" alt="Logo" class="w-8 h-8" />
-      Almoxarifado Online
+      Smart Nurse
     </NuxtLink>
 
     <div class="flex space-x-1 lg:space-x-6">
       <ClientOnly v-if="isAuthenticated">
-        <UButton @click="logout" color="red">
-          Deslogar
-        </UButton>
+        <UButton @click="logout" color="red"> Deslogar </UButton>
       </ClientOnly>
 
       <ClientOnly>
-        <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-          class="bg-gray-300 hover:bg-gray-400 text-gray-900" @click="toggleTheme" />
+        <UButton
+          :icon="
+            isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+          "
+          class="text-gray-900"
+          @click="toggleTheme"
+        />
       </ClientOnly>
       <button class="lg:hidden">
         <!-- Ícone do menu hamburger aqui -->
@@ -26,10 +33,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed, onMounted, onUnmounted } from 'vue';
-import { useAuthStore } from '~/stores/authStore' // Importe a store
+import { defineComponent, ref, computed, onMounted, onUnmounted } from "vue";
+import { useAuthStore } from "~/stores/authStore"; // Importe a store
 
-const auth = useAuthStore() // Use a store
+const auth = useAuthStore(); // Use a store
 
 // Estado para controlar se o modal está aberto ou fechado
 const isOpen = ref(false);
@@ -37,31 +44,34 @@ const isOpen = ref(false);
 // Função para abrir o modal
 const openModal = () => {
   isOpen.value = true;
-}
+};
 
 // Função para fechar o modal
 const closeModal = () => {
   isOpen.value = false;
-}
+};
 
 async function logout() {
   try {
     const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${auth.token}`  // Adicionando o token ao cabeçalho
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${auth.token}`, // Adicionando o token ao cabeçalho
     };
 
-    const { data, error } = await useFetch(`${import.meta.env.VITE_BASE_API_URL}/logout`, {
-      method: 'POST',
-      headers: headers
-    });
+    const { data, error } = await useFetch(
+      `${import.meta.env.VITE_BASE_API_URL}/logout`,
+      {
+        method: "POST",
+        headers: headers,
+      }
+    );
 
     if (data) {
       auth.logout();
-      return navigateTo('/login')
+      return navigateTo("/login");
     } else if (error) {
       console.error("Erro ao deslogar:", error);
-      return navigateTo('/login')
+      return navigateTo("/login");
     }
   } catch (error) {
     console.error(error);
@@ -69,7 +79,7 @@ async function logout() {
 }
 
 export default defineComponent({
-  name: 'Navbar',
+  name: "Navbar",
   setup() {
     const store = useAuthStore();
     const isAuthenticated = computed(() => auth.isAuthenticated);
@@ -77,16 +87,16 @@ export default defineComponent({
     const colorMode = useColorMode();
     const isDark = computed({
       get() {
-        return colorMode.value === 'dark';
+        return colorMode.value === "dark";
       },
       set(val) {
-        colorMode.preference = val ? 'dark' : 'light';
-      }
+        colorMode.preference = val ? "dark" : "light";
+      },
     });
 
     const toggleTheme = () => {
       isDark.value = !isDark.value;
-    }
+    };
 
     const isScrolled = ref(false);
 
@@ -96,14 +106,14 @@ export default defineComponent({
       } else {
         isScrolled.value = false;
       }
-    }
+    };
 
     onMounted(() => {
-      window.addEventListener('scroll', handleScroll);
+      window.addEventListener("scroll", handleScroll);
     });
 
     onUnmounted(() => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     });
 
     return {
@@ -115,7 +125,7 @@ export default defineComponent({
       isOpen,
       openModal,
       closeModal,
-    }
-  }
+    };
+  },
 });
 </script>
