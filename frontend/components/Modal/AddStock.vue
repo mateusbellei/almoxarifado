9187f09db2f4f8002f48f1b238320945e82f3203
mateusbellei <dev.mbellei@gmail.com>
2024-12-13 02:24:56 -0300

    formatted addStock

--- frontend/components/Modal/AddStock.vue
+++ frontend/components/Modal/AddStock.vue
@@ -5,7 +5,12 @@
         <template #header>
           <h1 class="text-primary font-medium">Adicionar Estoque</h1>
         </template>
-        <UForm class="space-y-4" :validate="validate" :state="state" @submit="submit">
+        <UForm
+          class="space-y-4"
+          :validate="validate"
+          :state="state"
+          @submit="submit"
+        >
           <UFormGroup label="Produto" name="produto">
             <UInput v-model="state.produto" />
           </UFormGroup>
@@ -40,34 +45,48 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, defineProps, defineEmits } from 'vue';
-import type { FormError, FormSubmitEvent } from '@nuxt/ui/dist/runtime/types';
-import { useAuthStore } from '~/stores/authStore';
+import { ref, watch, defineProps, defineEmits } from "vue";
+import type { FormError, FormSubmitEvent } from "@nuxt/ui/dist/runtime/types";
+import { useAuthStore } from "~/stores/authStore";
 
 const auth = useAuthStore();
 
 // Props
 const props = defineProps({
-  modelValue: Boolean
+  modelValue: Boolean,
 });
 
 // State
 const state = ref({
-  produto: '',
+  produto: "",
   unidade_medida: undefined,
   estoque: undefined,
   validade: undefined,
 });
 
-const successMessage = ref('');
-const errorMessage = ref('');
+const successMessage = ref("");
+const errorMessage = ref("");
 
 // Units
-const unidades = ['Unidade', 'Pacote', 'Rolo', 'Caixa', 'Bloco', 'Maço', 'Metro', 'Frasco', 'Tubo', 'Galão'];
+const unidades = [
+  "Unidade",
+  "Pacote",
+  "Rolo",
+  "Caixa",
+  "Bloco",
+  "Maço",
+  "Metro",
+  "Frasco",
+  "Tubo",
+  "Galão",
+];
 
 // Modal control
 const localIsOpen = ref(props.modelValue);
-watch(() => props.modelValue, newVal => localIsOpen.value = newVal);
+watch(
+  () => props.modelValue,
+  (newVal) => (localIsOpen.value = newVal)
+);
 
 // Emits
 const emit = defineEmits(["update:modelValue", "product-added"]);
@@ -78,32 +97,37 @@ const closeModal = () => updateModelValue(false);
 
 const validate = (state: any): FormError[] => {
   const errors = [];
-  if (!state.produto) errors.push({ path: 'produto', message: 'Required' });
-  if (!state.unidade_medida) errors.push({ path: 'unidade_medida', message: 'Required' });
-  if (!state.estoque) errors.push({ path: 'estoque', message: 'Required' });
-  if (state.estoque < 0) errors.push({ path: 'estoque', message: 'Não pode ser negativo' });
+  if (!state.produto) errors.push({ path: "produto", message: "Required" });
+  if (!state.unidade_medida)
+    errors.push({ path: "unidade_medida", message: "Required" });
+  if (!state.estoque) errors.push({ path: "estoque", message: "Required" });
+  if (state.estoque < 0)
+    errors.push({ path: "estoque", message: "Não pode ser negativo" });
   return errors;
-}
+};
 
 async function submit(event: FormSubmitEvent<any>) {
   try {
-    const { data, error }: { data: any, error: any } = await useFetch(`${import.meta.env.VITE_BASE_API_URL}/product`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${auth.token}`
-      },
-      body: JSON.stringify(event.data)
-    });
+    const { data, error }: { data: any; error: any } = await useFetch(
+      `${import.meta.env.VITE_BASE_API_URL}/product`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.token}`,
+        },
+        body: JSON.stringify(event.data),
+      }
+    );
 
     if (data.value) {
       console.log(data.value);
-      state.value.produto = '';
+      state.value.produto = "";
       state.value.unidade_medida = undefined;
       state.value.estoque = undefined;
       state.value.validade = undefined;
       successMessage.value = data.value.message;
-      errorMessage.value = '';
+      errorMessage.value = "";
       emit("product-added");
       setTimeout(() => {
         closeModal();
@@ -111,7 +135,7 @@ async function submit(event: FormSubmitEvent<any>) {
     } else if (error) {
       console.error(error.value.data.message);
       errorMessage.value = error.value.data.message;
-      successMessage.value = '';
+      successMessage.value = "";
     }
   } catch (error) {
     console.error(error);
@@ -122,10 +146,12 @@ async function submit(event: FormSubmitEvent<any>) {
 watch(localIsOpen, updateModelValue);
 
 // Passar nome do produto sempre pra todas letras maiusculas caso o valor seja diferente de undefined
-watch(() => state.value.produto, (newVal: string | undefined) => {
-  if (newVal) {
-    state.value.produto = newVal.toUpperCase();
+watch(
+  () => state.value.produto,
+  (newVal: string | undefined) => {
+    if (newVal) {
+      state.value.produto = newVal.toUpperCase();
+    }
   }
-});
-
+);
 </script>
